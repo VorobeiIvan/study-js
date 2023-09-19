c8245529f0d054ece51639577cf80effbc2c6756
Ivan <127958813+VorobeiIvan@users.noreply.github.com>
2023-09-19 17:24:43 +0300

    add answer

--- practic.js
+++ practic.js
@@ -2449,17 +2449,57 @@
 //     price: 2153,
 //   };
 //   // Change code below this line
-//   const keys = apartment;
-//   const values = apartment;
+//   const keys = Object.keys(apartment);
+//   const values = Object.values(apartment);
   
 
+// // _______________________________________________________________________________________________________________
+// Напиши функцію countTotalSalary(salaries), яка приймає об'єкт зарплат, де ім'я властивості - це ім'я співробітника, а значення властивості - це зарплата. Функція повинна розрахувати загальну суму зарплат співробітників і повернути її. Використовуй змінну totalSalary для зберігання загальної суми зарплати.
+
+// Оголошена функція countTotalSalary(salaries)
+// Виклик countTotalSalary({}) повертає 0
+// Виклик countTotalSalary({ mango: 100, poly: 150, alfred: 80 }) повертає 330
+// Виклик countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }) повертає 400
+// Функція враховує тільки власні властивості об'єкта
+
+// function countTotalSalary(salaries) {
+//     let totalSalary = 0;
+//     // Change code below this line
+//   let values = Object.values(salaries);
+//     for (let i=0; i<values.length; i+=1){
+//       totalSalary += values[i] 
+    
+//     }
+//      return totalSalary;
+//     // Change code above this line
+    
+//   }
+  
+//   console.log(countTotalSalary({ mango: 100, poly: 150, alfred: 80 }));
+//   console.log(countTotalSalary({ kiwi: 200, poly: 50, ajax: 150 }));
+  
 // _______________________________________________________________________________________________________________
 
+// Перебери масив об'єктів colors, використовуючи цикл for...of. Додай у масив hexColors значення властивостей hex, а в масив rgbColors - значення властивостей rgb з усіх об'єктів масиву colors.
 
+// Оголошена змінна colors
+// Значення змінної colors - це масив
+// Оголошена змінна hexColors
+// Значення змінної hexColors - це масив ["#f44336", "#2196f3", "#4caf50", "#ffeb3b"]
+// Оголошена змінна rgbColors
+// Значення змінної rgbColors - це масив ["244,67,54", "33,150,243", "76,175,80", "255,235,59"]
 
-// _______________________________________________________________________________________________________________
-
-
+const colors = [
+    { hex: "#f44336", rgb: "244,67,54" },
+    { hex: "#2196f3", rgb: "33,150,243" },
+    { hex: "#4caf50", rgb: "76,175,80" },
+    { hex: "#ffeb3b", rgb: "255,235,59" },
+  ];
+  
+  const hexColors = [];
+  const rgbColors = [];
+  // Change code below this line
+  
 
 // _______________________________________________________________________________________________________________
 
