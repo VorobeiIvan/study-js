8a36c38f4f647e2db960d413889f357e853f2bb2
Ivan <0675361905vi@gmail.com>
2023-09-27 11:39:33 +0300

    aad new task

--- practic.js
+++ practic.js
@@ -7,21 +7,61 @@
 
 // const [a, b, ...arr] = sourceWithoutFirstTwo;
 // console.log(arr);
-const result = {
-  success: ["max-length", "no-amd", "prefer-arrow-functions"],
-  failure: ["no-var", "var-on-top", "linebreak"],
-  skipped: ["no-extra-semi", "no-dup-keys"],
-};
-function makeList(arr) {
-  // Змініть код лише під цим рядком
-  const { success, failure, skipped } = result;
-  const failureItems = [];
-  for (let i = 0; i < failure.length; i++) {
-    failureItems.push(`<li class="text-warning">${failure[i]}</li>`);
-  }
-  // Змініть код лише над цим рядком
+// const result = {
+//   success: ["max-length", "no-amd", "prefer-arrow-functions"],
+//   failure: ["no-var", "var-on-top", "linebreak"],
+//   skipped: ["no-extra-semi", "no-dup-keys"],
+// };
+// function makeList(arr) {
+//   // Змініть код лише під цим рядком
+//   const { success, failure, skipped } = result;
+//   const failureItems = [];
+//   for (let i = 0; i < failure.length; i++) {
+//     failureItems.push(`<li class="text-warning">${failure[i]}</li>`);
+//   }
+//   // Змініть код лише над цим рядком
 
-  return console.log(failureItems);
+//   return console.log(failureItems);
+// }
+
+// const failuresList = makeList(result.failure);
+// const pizzaPalace = {
+//   pizzas: ["Ultracheese", "Smoked", "Four meats"],
+//   order(pizzaName, onSuccess, onError) {
+//     if (!this.pizzas.includes(pizzaName)) {
+//       return onError(`There is no pizza with a name ${pizzaName} in the assortment.`);
+//     } else {
+//       return onSuccess(`Your order is accepted. Cooking pizza ${pizzaName}.`);
+//     }
+//   },
+// };
+
+// // Callback for onSuccess
+// function makePizza(pizzaName) {
+//   return pizzaName;
+// }
+
+// // Callback for onError
+// function onOrderError(error) {
+//   return `Error! ${error}`;
+// }
+
+// // Method calls with callbacks
+// console.log(pizzaPalace.order("Smoked", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Four meats", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Big Mike", makePizza, onOrderError));
+// console.log(pizzaPalace.order("Vienna", makePizza, onOrderError));
+
+function calculateTotalPrice(orderedItems) {
+  let totalPrice = 0;
+  // Change code below this line
+
+orderedItems.forEach(function collback(number,index){
+  console.log(totalPrice+=number);
+});
+  // Change code above this line
+  return totalPrice;
 }
-
-const failuresList = makeList(result.failure);
+calculateTotalPrice([12, 85, 37, 4]) // повертає 138
+calculateTotalPrice([164, 48, 291])//повертає 503
+calculateTotalPrice([412, 371, 94, 63, 176]) //повертає 1116
