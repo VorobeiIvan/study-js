93adf740581dd3cf9847d958a220c4ba3dc3a9ab
VorobeiIvan <0675361905vi@gmail.com>
2023-11-25 16:12:05 +0200

    upd

--- practic.js
+++ practic.js
@@ -1072,47 +1072,46 @@
 
 // console.log(convertToRoman(36)); // "XXXVI"
 
-function rot13(str) {
-  const englishAlphabet = {
-    A: 1,
-    B: 2,
-    C: 3,
-    D: 4,
-    E: 5,
-    F: 6,
-    G: 7,
-    H: 8,
-    I: 9,
-    J: 10,
-    K: 11,
-    L: 12,
-    M: 13,
-    N: 14,
-    O: 15,
-    P: 16,
-    Q: 17,
-    R: 18,
-    S: 19,
-    T: 20,
-    U: 21,
-    V: 22,
-    W: 23,
-    X: 24,
-    Y: 25,
-    Z: 26,
-  };
+// function rot13(str) {
+//   const englishAlphabet = {
+//     A: 1,
+//     B: 2,
+//     C: 3,
+//     D: 4,
+//     E: 5,
+//     F: 6,
+//     G: 7,
+//     H: 8,
+//     I: 9,
+//     J: 10,
+//     K: 11,
+//     L: 12,
+//     M: 13,
+//     N: 14,
+//     O: 15,
+//     P: 16,
+//     Q: 17,
+//     R: 18,
+//     S: 19,
+//     T: 20,
+//     U: 21,
+//     V: 22,
+//     W: 23,
+//     X: 24,
+//     Y: 25,
+//     Z: 26,
+//   };
 
-  
-  let keys = Object.keys(englishAlphabet);
-  keys.forEach(function (key) {
-    let value = englishAlphabet[key];
-    value += 13;
-    if (value > 26) {
-      value -= 26;
-    }
-    console.log(key, value);
-  });
-  return str;
-}
+//   let keys = Object.keys(englishAlphabet);
+//   keys.forEach(function (key) {
+//     let value = englishAlphabet[key];
+//     value += 13;
+//     if (value > 26) {
+//       value -= 26;
+//     }
+//     console.log(key, value);
+//   });
+//   return str;
+// }
 
-console.log(rot13("SERR PBQR PNZC"));
+// console.log(rot13("SERR PBQR PNZC"));
